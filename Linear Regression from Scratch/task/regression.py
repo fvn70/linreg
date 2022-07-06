b5bc62882631d74ee5bb8005c3cadcf2a61056b3
fvn70 <fvn_reg@mail.ru>
2022-07-06 13:24:41 +0700

    Stage 2/4: Multiple linear regression and predictions

--- Linear Regression from Scratch/task/regression.py
+++ Linear Regression from Scratch/task/regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 class CustomLinearRegression:
@@ -9,20 +10,30 @@ class CustomLinearRegression:
         self.intercept = 0.0
 
     def fit(self, X, y):
-        Xt = np.transpose(X)
-        m2 = np.linalg.inv(np.dot(Xt, X))
-        m3 = np.dot(m2, Xt)
-        beta = np.dot(m3, y)
-        return beta
+        X = X.to_numpy()
+        y = y.to_numpy()
+        if self.fit_intercept:
+            X = np.matrix([[1, i] for i in X])
+        Xt = X.T
+        beta = np.linalg.inv(Xt @ X) @ Xt @ y
+        self.coefficient = beta
+        if self.fit_intercept:
+            self.intercept = beta[0, 0]
+            self.coefficient = beta[0, 1]
+
+    def predict(self, X):
+        X = X.to_numpy()
+        y = X @ self.coefficient
+        return y
 
 
-x = np.array([4.0, 4.5, 5, 5.5, 6.0, 6.5, 7.0])
-y = np.array([33, 42, 45, 51, 53, 61, 62])
-X = np.matrix([[1, i] for i in x])
+dic = {'x': [4, 4.5, 5, 5.5, 6, 6.5, 7],
+       'w': [1, -3, 2, 5, 0, 3, 6],
+       'z': [11, 15, 12, 9, 18, 13, 16],
+       'y': [33, 42, 45, 51, 53, 61, 62]}
+df = pd.DataFrame(dic)
 
-model = CustomLinearRegression().fit(X, y)
-b0 = model[0, 0]
-b1 = model[0, 1]
-dic = f"{'{'}'Intercept': {b0}, 'Coefficient': array([{round(b1, 7)}]){'}'}"
-print(dic)
-
+regCustom = CustomLinearRegression(fit_intercept=False)
+regCustom.fit(df[['x', 'w', 'z']], df['y'])
+y_pred = regCustom.predict(df[['x', 'w', 'z']])
+print(y_pred)
